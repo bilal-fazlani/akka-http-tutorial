07b89d68a0b1f741b31375325ae38c92f5a004e9
Bilal Fazlani <bilal.m.fazlani@gmail.com>
2018-10-29 18:54:05 +0530

    handle failure on bind

--- src/main/scala/example/VIII_ServerBinding.scala
+++ src/main/scala/example/VIII_ServerBinding.scala
@@ -3,7 +3,7 @@ package example
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.{HttpEntity, HttpRequest, HttpResponse}
 
-import scala.util.Random
+import scala.util.{Random, Success}
 
 object VIII_ServerBinding extends App with AkkaSystem {
 
@@ -13,7 +13,14 @@ object VIII_ServerBinding extends App with AkkaSystem {
     .bind("localhost", 8080)
     //add a printline hook
     .mapMaterializedValue(bindingFuture => {
-      bindingFuture.onComplete(_ => println("server started..."))
+      bindingFuture
+        .onComplete {
+          case Success(_) =>
+            println("server started at port 8080")
+          case scala.util.Failure(exception) =>
+            println("could not bind port 8080")
+            system.terminate()
+        }
       bindingFuture
     })
     //handle every connection
